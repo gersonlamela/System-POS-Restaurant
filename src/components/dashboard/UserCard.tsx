146f74914098b0c127cdbd2f42920c53a2dcbb90
Gerson Lamela <gersonlamela7@gmail.com>
2024-03-18 20:10:41 +0000

    style: add color

--- src/components/dashboard/UserCard.tsx
+++ src/components/dashboard/UserCard.tsx
@@ -1,19 +1,24 @@
 'use client'
 
+import { getRole } from '@/functions/user/user'
+import { get } from 'http'
 import { LogOut } from 'lucide-react'
 import { signOut, useSession } from 'next-auth/react'
 
 export default function UserCard() {
   const { data: session } = useSession()
   return (
-    <div className="flex flex-col items-center justify-center gap-2 md:flex-row ">
+    <div className="mb-4 flex flex-col items-center justify-center gap-2 md:flex-row ">
       <div className="flex flex-col items-center justify-center ">
-        <div>{session?.user.name}</div>
-        <div> {session?.user.email}</div>
+        <div className="text-xl font-semibold">{session?.user.name}</div>
+        <div className="text-base font-medium capitalize  text-secondary-foreground">
+          {' '}
+          {getRole((session as any)?.user.role)}
+        </div>
       </div>
 
       <button onClick={() => signOut()}>
-        <LogOut size={17} />
+        <LogOut size={25} className="hover:text-red-500" />
       </button>
     </div>
   )
