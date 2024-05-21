be1e29ac34ae816f31ce5108de4cf8a880800cba
Gerson Lamela <gersonlamela7@gmail.com>
2024-05-21 21:41:27 +0100

    feat: try send orders with getServerSideProps

--- src/app/dashboard/orders/page.tsx
+++ src/app/dashboard/orders/page.tsx
@@ -1,8 +1,17 @@
 import TableOrders from '@/components/dashboard/Table/TableOrders/TableOrders'
 import { handleGetOrders } from '@/functions/Order/order'
+import { Order } from '@/types/Order'
 
-export default async function page() {
+export async function getServerSideProps() {
   const orders = await handleGetOrders()
 
+  return {
+    props: {
+      orders,
+    },
+  }
+}
+
+export default function Page({ orders }: { orders: Order[] }) {
   return <TableOrders orders={orders} />
 }
